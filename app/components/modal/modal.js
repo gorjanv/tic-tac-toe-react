02c468512ea268cb0a1d4adbe8459d53ef3fcdb7
gorjanv <gorjanv@gmail.com>
2017-02-03 20:05:14 +0100

    added some styling for the end game modal

--- app/components/modal/modal.js
+++ app/components/modal/modal.js
@@ -5,16 +5,17 @@ export default class Modal extends Component {
     render() {
         return (
           
-          <div className="modal-container">
-              <div className="overlay"></div>
-              {this.props.gameState === "draw" ?
-                <div key={0} className="modal-header">Game drawn ..</div> :
-                <div key={0} className="modal-header">{"Player " + this.props.player + " has won"}</div> }
-                <div key={1} className="modal-body">{"Would you like a new game?"}</div>
-      
-              <div key={2} className="modal-footer">
-                  <input key={3} className="dialog-button" type="button" onClick={this.props.endGameButton} value="YES"/>
-              </div>
+          <div className="modal">
+                <div className="modal-content">
+                {this.props.gameState === "draw" ?
+                    <div key={0} className="modal-header">Game drawn ..</div> :
+                    <div key={0} className="modal-header">{"Player " + this.props.player + " has won"}</div> }
+                    <div key={1} className="modal-body">{"Would you like a new game?"}</div>
+          
+                    <div key={2} className="modal-footer">
+                      <input key={3} className="modal-button" type="button" onClick={this.props.endGameButton} value="YES"/>
+                    </div>
+                </div>
           </div>
         );
     }
